ec2b87d849bffc85b6af9ea26be54a92797e927b
raphaelattfield <raphaelattfield@hotmail.co.uk>
2023-11-30 19:35:18 -0500

    working with mongoose

--- server.js
+++ server.js
@@ -10,18 +10,16 @@ const mongoose = require("mongoose");
 
 const upload = multer({ dest: __dirname + "/public/images" });
 
-
 mongoose
-  .connect(
-    "mongodb+srv://raphaelattfield:U4ZivtKn7i6x-iY@cluster0.3umc0ij.mongodb.net/?retryWrites=true&w=majority"
+.connect(
+    "mongodb+srv://raphaelattfield:U4ZivtKn7i6x-iY@cluster0.3umc0ij.mongodb.net/"
   )
   .then(() => console.log("Connected to mongodb..."))
-  .catch((err) => console.error("could not connect ot mongodb...", err));
+  .catch((err) => console.error("could not connect to mongodb...", err));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
-
 
 const teamSchema = new mongoose.Schema({
     name: String,
@@ -31,110 +29,87 @@ const teamSchema = new mongoose.Schema({
     nickname: String,
     kitcolour: String,
     players: [String],
-  });
+  /*  _id: mongoose.SchemaTypes.ObjectId*/
+});
 
-  const Team = mongoose.model("Team", teamSchema);
+const Team = mongoose.model("Team", teamSchema);
 
-  app.get("/api/recipes", (req, res) => {
-    getTeams(res);
-  });
+app.get("/api/teams", (req, res) => {
+  getTeams(res);
+});
 
-  const getTeams = async (res) => {
-    const teams = await Team.find();
-    res.send(teams);
-  };
-
-
-
+const getTeams = async (res) => {
+  const teams = await Team.find();
+  res.send(teams);
+};
 
 app.post("/api/teams", upload.single("img"), (req, res) => {
-    const result = validateTeam(req.body);
+  const result = validateTeam(req.body);
 
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message); //changed information to details
-        return;
-    }
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
 
-    const team = {
-        _id: teams.length + 1,
+  const team = new Team({
         name: req.body.name,
         position: req.body.position,
         country: req.body.county,
         nickname: req.body.nickname,
         kitcolour: req.body.kitcolour,
-        players: req.body.players.split(",")
-    }
+        players: req.body.players.split(","),
+  });
 
-    if (req.file) {
-        team.img = "images/" + req.file.filename;
-    }
-    teams.push(team);
-    res.send(teams);
+  if (req.file) {
+    team.img = "images/" + req.file.filename;
+  }
+
+  createTeam(team, res);
 });
 
-
-
-
-
+const createTeam = async (team, res) => {
+  const result = await team.save();
+  res.send(team);
+};
 
 app.put("/api/teams/:id", upload.single("img"), (req, res) => {
-    const id = parseInt(req.params.id);
+  const result = validateTeam(req.body);
 
-    const team = teams.find((r) => r._id === id);;
-    const result = validateTeam(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
 
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    team.name = req.body.name;
-    team.position = req.body.position;
-    team.country = req.body.county;
-    team.nickname = req.body.nickname;
-    team.kitcolour = req.body.kitcolour;
-    team.players = req.body.players.split(",");
-
-    if (req.file) {
-        team.img = "images/" + req.file.filename;
-    }
-
-    res.send(team);
+  updateTeam(req, res);
 });
 
+const updateTeam = async (req, res) => {
+  let fieldsToUpdate = {
+    name: req.body.name,
+    position: req.body.position,
+    country: req.body.county,
+    nickname: req.body.nickname,
+    kitcolour: req.body.kitcolour,
+    players: req.body.players.split(","),
+  };
+
+  if (req.file) {
+    fieldsToUpdate.img = "images/" + req.file.filename;
+  }
+
+  const result = await Team.updateOne({ _id: req.params.id }, fieldsToUpdate);
+  const team = await Team.findById(req.params.id);
+  res.send(team);
+};
 
 app.delete("/api/teams/:id", upload.single("img"), (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const teamIndex = teams.findIndex((team) => team._id === id);
-
-    if (teamIndex === -1) {
-        res.status(404).send("The team was not found");
-        return;
-    }
-
-    teams.splice(teamIndex, 1);
-    res.send(teams);
-    //const team = teams.find((r) => r._id === id);
-    // if (!team) {
-    //     res.status(404).send("The team was not found");
-    //     return;
-    // }
-    // const index = teams.indexOf(team);
-    // if (index === -1) {
-    //     res.status(404).send("The team was not found");
-    //     return;
-    // }
-    // team.splice(index, 1);
-    // res.send(team);
-
+  removeTeam(res, req.params.id);
 });
 
-
-
-
-
-
+const removeTeam = async (res, id) => {
+  const team = await Team.findByIdAndDelete(id);
+  res.send(team);
+};
 
 const validateTeam = (team) => {
     const schema = Joi.object({
@@ -147,10 +122,9 @@ const validateTeam = (team) => {
         players: Joi.allow(""),
     });
 
-    return schema.validate(team);
+  return schema.validate(team);
 };
 
 app.listen(3000, () => {
-    console.log("Listening");
+  console.log("I'm listening");
 });
-
